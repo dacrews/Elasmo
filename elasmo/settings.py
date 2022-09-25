f06c4309e531cf976f485dfe1aa398f8b0fdfad0
dacrews <70422954+dacrews@users.noreply.github.com>
2022-09-25 01:46:21 -0400

    Update settings.py

--- elasmo/settings.py
+++ elasmo/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = *HIDDEN*
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://dacrews.pythonanywhere.com/index']
 
 
 # Application definition
