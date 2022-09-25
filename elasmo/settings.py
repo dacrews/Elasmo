5fd54182be9f509442016fe852ada5483c81f377
dacrews <70422954+dacrews@users.noreply.github.com>
2022-09-25 01:27:24 -0400

    Update settings.py

--- elasmo/settings.py
+++ elasmo/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o_l-49dm@&$(!g%xh=jg4(g=clxoo6ptuc&52g0o&=*3%d-)c&'
+# SECRET_KEY = *HIDDEN*
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
